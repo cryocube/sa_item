22370c2d750e0467d7e1110e6457b5db5dcc1a61
Kyle Ricks <ricksk3@wwu.edu>
2017-07-13 18:24:20 -0700

    Initial .py file and version 1 sqlite3 db

---
+++ item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Shadow Accord Item for creating Magic Item Loadouts
+#
+# Original Author: Kyle Ricks
+# Organization: Shadow Accord - NW LARPers
+#
+# Imports
+#
+import sys
+import sqlite3
+#
+#
+
